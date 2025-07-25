d20f5624ff3e195828746f5b1f2c50ac84c0e8d7
Smitraj <smitrajbankar11@gmail.com>
2025-07-25 23:33:36 +0530

    order Model Added

---
+++ server/database/models/order-model.js
@@ -0,0 +1,71 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+// Sub-schema for individual ordered items
+const orderProductSchema = new Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+});
+
+// Main Order Schema
+const orderSchema = new Schema(
+  {
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendors",
+      required: true,
+    },
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users", // Reference the User model
+      required: true,
+    },
+    products: {
+      type: [orderProductSchema],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      default: 0,
+      set: (v) => Math.round(v * 100) / 100, // Round to 2 decimal places
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["Pending", "Paid", "Unpaid"],
+      default: "Pending",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["Cash", "Online", "Counter", "Pending", "Razorpay"],
+      default: "Cash",
+    },
+    paymentId: {
+      type: String,
+      default: "",
+    },
+    paymentOrderId: {
+      type: String,
+      default: "",
+    },
+    trackId:{
+        type:String,
+        default:""
+    },
+    status: {
+      type: String,
+      enum: ["Not Process", "Processing", "Delivered", "Cancelled"],
+      default: "Not Process",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = new mongoose.model("Order", orderSchema);
+module.exports = Order;
